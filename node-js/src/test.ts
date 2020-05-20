eef802897f1b0fe2a382e6778361d276ffaec3c2
Dr-NULL <silvaaguilarfelipe@gmail.com>
2020-05-20 12:41:15 -0400

    Add the *.exe files to the package

--- node-js/src/test.ts
+++ node-js/src/test.ts
@@ -4,7 +4,7 @@ import { join } from "path";
 (async function() {
     try {
         const filename = join(__dirname, "..", "test", "test-15x10.pdf")
-        CmdPrinter.printRemote(
+        await CmdPrinter.printRemote(
           [ filename ],
           [ '\\\\NB-FSILVA\\EasyCoder PX4i (203 dpi) - DP 100 x 150' ]
         )
